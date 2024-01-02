7a36daa271196a7448bcddb9f64c6ad727a4d4d1
Wasivis <ivanmontequin@gmail.com>
2024-01-02 18:40:47 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +12,26 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
 
+const upload = multer({ storage: storage });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
